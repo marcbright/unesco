81c0c0d62920f2cac502af10bb4cb40722d51bfd
marcbright <marcbright2015@gmail.com>
2024-10-15 09:00:07 +0000

    added cleaned df

--- pages/unesco_post.py
+++ pages/unesco_post.py
@@ -11,20 +11,21 @@ logo_path = "pages/gi_logo.png"
 # Display the logo
 st.logo(logo_path, size="large", link = "https://gi-kace.gov.gh/")
 
-df_2 = pd.read_excel("UNESCOs_Technology_Enabled_Open_School_Project_TeOSS_Learners_Training_Program_Post-Training_Assessment_.xlsx")
-df_drop = df_2.drop(["4. Grade/Class", "5. Region of School", "6. How would you rate the overall training experience?",
-             "7. What aspects of the training did you enjoy most?", "8. Were the training objectives clearly communicated?", "9. How engaging did you find the training sessions?",
-             "10. How effective were the trainers in delivering the content?", "11. How would you rate the training materials provided?", "12. What new skills or knowledge did you acquire from the training?", 
-             "13. How confident do you feel in using technology for learning after the training?", "14. What challenges did you encounter while using technology during the training?",
-             "15. How do you plan to apply what you learned in your studies?", "16. What topics would you like to see covered in future training sessions?",
-             "17. Do you have any suggestions to improve the training program?", "18. Any additional comments or feedback?", "_id", "_uuid", "_submission_time", "_validation_status", 
-             "_notes", "_status", "_submitted_by", "__version__", "_tags", "_index"], axis = 1, inplace=True)
+#df_2 = pd.read_excel("UNESCOs_Technology_Enabled_Open_School_Project_TeOSS_Learners_Training_Program_Post-Training_Assessment_.xlsx")
+#df_drop = df_2.drop(["4. Grade/Class", "5. Region of School", "6. How would you rate the overall training experience?",
+#             "7. What aspects of the training did you enjoy most?", "8. Were the training objectives clearly communicated?", "9. How engaging did you find the training sessions?",
+#             "10. How effective were the trainers in delivering the content?", "11. How would you rate the training materials provided?", "12. What new skills or knowledge did you acquire from the training?", 
+#             "13. How confident do you feel in using technology for learning after the training?", "14. What challenges did you encounter while using technology during the training?",
+#             "15. How do you plan to apply what you learned in your studies?", "16. What topics would you like to see covered in future training sessions?",
+#             "17. Do you have any suggestions to improve the training program?", "18. Any additional comments or feedback?", "_id", "_uuid", "_submission_time", "_validation_status", 
+#             "_notes", "_status", "_submitted_by", "__version__", "_tags", "_index"], axis = 1, inplace=True)
+
+df_2 = pd.read_csv('unesco_cleaned_data_pro.csv')
+st.dataframe(df_2)
 
 
-
-
-st.markdown("### POST-ASSESMENT DATA")
-st.dataframe(df_2.head(), width=2000)
+#st.markdown("### POST-ASSESMENT DATA")
+#st.dataframe(df_2.head(), width=2000)
 
 
 
@@ -33,75 +34,75 @@ st.dataframe(df_2.head(), width=2000)
 st.markdown("### BELOW ARE THE CHARTS FOR THE POST ASSESMENT")
 st.markdown("**A bar chart representing the Overall Training Experience**")
 st.image("training_experience.png")
-training_experience = df_2["7. How would you rate the overall training experience?"].value_counts()
+training_experience = df_2["how_would_you_rate_the_overall_training_experience?"].value_counts()
 st.write(training_experience)
 
 
 
 st.markdown("**A wordcloud chart representing What Aspects Of The Training Did You Enjoy Most**")
 st.image("training_you_enjoy_most.png")
-training_you_enjoy_most = df_2["8. What aspects of the training did you enjoy most?"].value_counts()
+training_you_enjoy_most = df_2["what_aspects_of_the_training_did_you_enjoy_most?"].value_counts()
 st.write(training_you_enjoy_most)
 
 
 st.markdown("**A bar chart representing Were The Training Objectives Clearly Communicated?**")
 st.image("training_objectives.png")
-training_objectives = df_2["9. Were the training objectives clearly communicated?"].value_counts()
+training_objectives = df_2["were_the_training_objectives_clearly_communicated?"].value_counts()
 st.write(training_objectives)
 
 
 st.markdown("**A bar chart representing How Engaging Did You Find The Training Sessions?**")
 st.image("engaging_training_sessions.png")
-engaging_training_sessions = df_2["10. How engaging did you find the training sessions?"].value_counts()
+engaging_training_sessions = df_2["how_engaging_did_you_find_the_training_sessions?"].value_counts()
 st.write(engaging_training_sessions)
 
 
 st.markdown("**A bar chart representing How Effective Were The Trainers In Delivering The Content?**")
 st.image("effective_delivering_the_content.png")
-effective_delivering_the_content = df_2["11. How effective were the trainers in delivering the content?"].value_counts()
+effective_delivering_the_content = df_2["how_effective_were_the_trainers_in_delivering_the_content?"].value_counts()
 st.write(effective_delivering_the_content)
 
 
 st.markdown("**A bar chart representing How Would You Rate The Training Materials Provided?**")
 st.image("training_materials_provided.png")
-training_materials_provided = df_2["12. How would you rate the training materials provided?"].value_counts()
+training_materials_provided = df_2["how_would_you_rate_the_training_materials_provided?"].value_counts()
 st.write(training_materials_provided)
 
 
 st.markdown("**A wordcloud chart representing What New Skills Or Knowledge Did You Acquire From The Training**")
 st.image("new_skills_or_knowledge.png")
-new_skills_or_knowledge = df_2["13. What new skills or knowledge did you acquire from the training?"].value_counts()
+new_skills_or_knowledge = df_2["what_new_skills_or_knowledge_did_you_acquire_from_the_training?"].value_counts()
 st.write(new_skills_or_knowledge)
 
 
 st.markdown("**A bar chart representing How Confident Do You Feel In Using Technology For Learning After The Training?**")
 st.image("confident_using_technology_after_training.png")
-confident_using_technology_after_training = df_2["14. How confident do you feel in using technology for learning after the training?"].value_counts()
+confident_using_technology_after_training = df_2["how_confident_do_you_feel_in_using_technology_for_learning_after_the_training?"].value_counts()
 st.write(confident_using_technology_after_training)
 
 
 
 st.markdown("**A wordcloud chart representing What Challenges Did You Encounter While Using Technology During The Training**")
 st.image("challenges_encounter.png")
-challenges_encounter = df_2["15. What challenges did you encounter while using technology during the training?"].value_counts()
+challenges_encounter = df_2["what_challenges_did_you_encounter_while_using_technology_during_the_training?"].value_counts()
 st.write(challenges_encounter)
 
 
 st.markdown("**A wordcloud chart representing How Do You Plan To Apply What You Learned In Your Studies**")
 st.image("learned_in_studies.png")
-learned_in_studies = df_2["16. How do you plan to apply what you learned in your studies?"].value_counts()
+learned_in_studies = df_2["how_do_you_plan_to_apply_what_you_learned_in_your_studies?"].value_counts()
 st.write(learned_in_studies)
 
 
 st.markdown("**A wordcloud chart representing What Topics Would You Like To See Covered In Future Training Sessions**")
 st.image("topics_in_future_training_sessions.png")
-topics_in_future_training_sessions = df_2["17. What topics would you like to see covered in future training sessions?"].value_counts()
+topics_in_future_training_sessions = df_2["what_topics_would_you_like_to_see_covered_in_future_training_sessions?"].value_counts()
 st.write(topics_in_future_training_sessions)
 
 
 st.markdown("**A wordcloud chart representing Do You Have Any Suggestions To Improve The Training Program**")
 st.image("suggestions_to_improve.png")
-suggestions_to_improve = df_2["18. Do you have any suggestions to improve the training program?"].value_counts()
+suggestions_to_improve = df_2["do_you_have_any_suggestions_to_improve_the_training_program?"].value_counts()
 st.write(suggestions_to_improve)
 
 
--- pages/unesco_pre.py
+++ pages/unesco_pre.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import streamlit as st
+import csv
 
 
 logo_path = "pages/gi_logo.png"
@@ -11,85 +12,89 @@ logo_path = "pages/gi_logo.png"
 # Display the logo
 st.logo(logo_path, size="large", link = "https://gi-kace.gov.gh/")
 
-st.markdown("## UNESCO Baseline and Post assesment")
-df_1 = pd.read_excel('UNESCO Technology Baseline Assesment.xlsx', engine='openpyxl')
+#st.markdown("## UNESCO Baseline and Post assesment")
+#df_1 = pd.read_excel('UNESCO Technology Baseline Assesment.xlsx', engine='openpyxl')
 
-st.markdown("### PRE-ASSESMENT DATA")
-st.dataframe(df_1.head(), width=2000)
+
+df_1 = pd.read_csv('unesco_cleaned_data.csv')
+st.dataframe(df_1)
+
+#st.markdown("### PRE-ASSESMENT DATA")
+#st.dataframe(df_1.head(), width=2000)
 
 st.markdown("### BELOW ARE THE CHARTS FOR THE BASELINE ASSESMENT")
 
 #Age Distribution
 st.markdown("**A bar chart representing the Age Distribution**")
 st.image("age.png")
-age_counts = df_1["2. Age"].value_counts()
+age_counts = df_1["age"].value_counts()
 st.write(age_counts)
 
 #Gender Distribution
 st.markdown("**A bar chart representing the Gender Distribution**")
 st.image("gender.png")
-gender_counts = df_1["3. Gender"].value_counts()
+gender_counts = df_1["gender"].value_counts()
 st.write(gender_counts)
 
 
 #Region of school
 st.markdown("**A bar chart representing the total number of students per a Region**")
 st.image("regionschool.png")
-region_counts = df_1["7. Region of School"].value_counts()
+region_counts = df_1["region_of_School"].value_counts()
 st.write(region_counts)
 
 
 st.markdown("**A bar chart representing the Current Grade Distribution**")
 st.image("current_grade.png")
-current_grade = df_1["6. Current Grade Level"].value_counts()
+current_grade = df_1["current_grade_level"].value_counts()
 st.write(current_grade)
 
 
 st.markdown("**A bar chart representing Devices Used for Online Studie**")
 st.image("devices_used_online_studies.png")
-devices_used_online_studies = df_1["If yes, what type of device do you use?"].value_counts()
+devices_used_online_studies = df_1["if_yes_what_type_of_device_do_you_use?"].value_counts()
 st.write(devices_used_online_studies)
 
 
 st.markdown("**A bar chart representing How Comfortable Are You With Using Digital Devices For Learning**")
 st.image("confi_devices.png")
-confi_devices = df_1["9. How comfortable are you with using digital devices for Learning?"].value_counts()
+confi_devices = df_1["how_comfortable_are_you_with_using_digital_devices_for_Learning?"].value_counts()
 st.write(confi_devices)
 
 
 st.markdown("**A bar chart representing Do You Have Internet Access?**")
 st.image("internet_access.png")
-internet_access = df_1["10. Do you have internet access?"].value_counts()
+internet_access = df_1["do_you_have_internet_access?"].value_counts()
 st.write(internet_access)
 
 st.markdown("**A bar chart representing What Type Of Internet Access Do You Use?**")
 st.image("if_yes_internet_access.png")
-if_yes_internet_access = df_1[" If yes, what type of internet access do you use?"].value_counts()
+if_yes_internet_access = df_1["if_yes_what_type_of_internet_access_do_you_use?"].value_counts()
 st.write(if_yes_internet_access)
 
 
 st.markdown("**A bar chart representing How Comfortable Are You Using The Internet To Access Information**")
 st.image("internet_to_access_information.png")
-internet_to_access_information = df_1["11. How comfortable are you using the internet to access information?"].value_counts()
+internet_to_access_information = df_1["how_comfortable_are_you_using_the_internet_to_access_information?"].value_counts()
 st.write(internet_to_access_information)
 
 
 
 st.markdown("**A bar chart representing What Is Your Preferred Learning Style**")
 st.image("preferred_learning_style.png")
-preferred_learning_style = df_1["12.What is your preferred learning style?"].value_counts()
+preferred_learning_style = df_1["what_is_your_preferred_learning_style?"].value_counts()
 st.write(preferred_learning_style)
 
 
 st.markdown("**A bar chart representing Have You Used An Online Learning Platform Before**")
 st.image("learning_platform_before.png")
-learning_platform_before = df_1["13. Have you used an online learning platform before? "].value_counts()
+learning_platform_before = df_1["have_you_used_an_online_learning_platform_before?"].value_counts()
 st.write(learning_platform_before)
 
 
 st.markdown("**A bar chart representing What Is Your Level Of Experience With Online Remote Learning Platforms**")
 st.image("experience_with_online.png")
-experience_with_online = df_1["14. What is your level of experience with online/remote learning platforms?"].value_counts()
+experience_with_online = df_1["what_is_your_level_of_experience_with_online_remote_learning_platforms?"].value_counts()
 st.write(experience_with_online)
 
 #st.markdown("**A bar chart representing What are the main challenges you have faced or expect to face with online learning**")
@@ -101,10 +106,10 @@ st.write(experience_with_online)
 
 st.markdown("**A bar chart representing Have you heard about the TeOSS project before?**")
 st.image("teOSS_project_before.png")
-teOSS_project_before = df_1["17. Have you heard about the TeOSS project before?"].value_counts()
+teOSS_project_before = df_1["have_you_heard_about_the_TeOSS_project_before?"].value_counts()
 st.write(teOSS_project_before)
 
 st.markdown("**A bar chart representing What are your expectations for the TeOSS Learners Training Program?**")
 st.image("your_expectations.png")
-your_expectations = df_1["18. What are your expectations for the TeOSS Learners Training Program?"]
+your_expectations = df_1["what_are_your_expectations_for_the_TeOSS_Learners_Training_Program?"]
 st.write(your_expectations)
